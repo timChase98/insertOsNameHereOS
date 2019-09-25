b0ccb532e97e589a75522014f9137ad43897b04a
timChase98 <tchase@umassd.edu>
2019-09-25 13:34:19 -0400

    Removed obscenities realted to windows.

--- insertOsNameHereOS/os.s
+++ insertOsNameHereOS/os.s
@@ -4,26 +4,24 @@
  *
  * Created: 9/18/2019 11:18:24 AM
  *  Author: tchase
- */ 
+ */
   #include <avr/io.h>
   .equ STARTOFTASKLIST, 0x0100
 
 
  .global os
 os:
-	
+
 	sbi _SFR_IO_ADDR(PINB), 5
 	ret
 
 .global TIMER4_COMPA_vect
 
 TIMER4_COMPA_vect:
-	pop R18;  get PC from stack 
+	pop R18;  get PC from stack
 	pop R19
 	sbi		 _SFR_IO_ADDR(PINE),0				;toggle PE.0
-	
+
 
 
     reti										;and done
-; fuck git on windows
-
